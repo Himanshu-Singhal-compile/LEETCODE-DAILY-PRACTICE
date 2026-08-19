3a2f7d077227ca8003e0115be1a8b78ea8d4734c
Himanshu-Singhal-compile <himanshusinghal822@gmail.com>
2026-08-19 22:44:04 +0530

    Time: 11 ms (95.27%) | Memory: 60.1 MB (73.48%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    int maxNumberOfFamilies(int n, vector<vector<int>>& seats ) {
-        int m = seats.size() ; 
-        sort( seats.begin() , seats.end() , []( const auto& a , const auto& b ){
-            return a[1] < b[1] ; 
-        }) ; 
-        long long last_seat = 0 ; 
-        long long ans = 0 ; 
-        map<int , vector<int> > row_wise ; 
-        for( int i = 0 ; i< m ; i++){
-            row_wise[seats[i][0]].push_back( seats[i][1] ) ; 
+    int maxNumberOfFamilies(int n, vector<vector<int>>& seats) {
+        unordered_map<int, int> rowMask; // row -> bitmask of reserved seat cols
+        for (auto& s : seats)
+            rowMask[s[0]] |= (1 << s[1]);
+
+        const int LEFT  = 0b0000111100; // seats 2,3,4,5
+        const int MID   = 0b0011110000; // seats 4,5,6,7
+        const int RIGHT = 0b1111000000; // seats 6,7,8,9
+        const int ALL   = LEFT | MID | RIGHT; // seats 2..9
+
+        // rows with no reservation at all -> 2 families each
+        long long ans = 2LL * (n - (int)rowMask.size());
+
+        for (auto& [row, mask] : rowMask) {
+            if ((mask & ALL) == 0) ans += 2;
+            else if ((mask & LEFT) == 0 || (mask & MID) == 0 || (mask & RIGHT) == 0) ans += 1;
         }
 
-        for( auto& p : row_wise ){
-            int current_seat = p.first ; 
-            vector<int> row_reserve_seat = p.second ; 
-            vector<int> vis ( 11 , 0 ) ; 
-            for( int s : row_reserve_seat )vis[s] =1 ; 
-
-            // check for all 3 cases 
-            int cnt = 0 ; 
-            if( !vis[2] && !vis[3] && !vis[4] && !vis[5] && !vis[6] && !vis[7] && !vis[8] && !vis[9] )cnt+=2 ; 
-            else if( !vis[2] && !vis[3] && !vis[4] && !vis[5] )cnt++ ; 
-            else if( !vis[4] && !vis[5] && !vis[6] && !vis[7] ) cnt++ ; 
-
-            else if(!vis[6] && !vis[7] && !vis[8] && !vis[9]) cnt++ ; 
-
-            ans+= cnt ; 
-            ans+= (current_seat - last_seat - 1)*2; 
-            last_seat = current_seat ; 
-        }
-        if( last_seat != n ){
-            ans+= ( n- last_seat)*2 ; 
-        }
-        return ( int)ans ; 
-
-
+        return (int)ans;
     }
 };
